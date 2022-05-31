85912cbf953b624d26bce2bd0c4f99d8a2e67a36
ivanbuzyka <ivanbuzyka@gmail.com>
2022-05-31 16:10:59 +0200

    restructuring the project again: removed simplified images for now

--- SolrVersions/8.1.1/HttpAndNoVolumes/solrCloudStart.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# The command to start the SolrCloud using integrated Zookeeper with one solr node
-solr-8.1.1/bin/solr start -f -cloud -p 9993 -s solr-8.1.1/example/cloud/node1/solr -force
